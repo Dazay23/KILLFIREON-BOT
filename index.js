d83e6bd3fb800de671433b1dcc3a82c89dfaeef8
Dazay23 <flavius.constantin.varvaroi@gmail.com>
2020-11-02 22:24:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-const { Client } = require('discord.js');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 bot.on("ready", function (){
-    console.log(`{bot.user.username} Is Online!`);
+    console.log(`${bot.user.username} Is Online`);
 });
 
-bot.on('message', msg => {
-    if (msg.content === '?links')
-      msg.reply("milsugi")
-});
+bot.login(process.env.token);
 
-bot.login('NzcwNDAxNzQzNTY0NzAxNzY2.X5dCiQ.7FGAOimuXD__syAH9PntwaBjlhQ')
